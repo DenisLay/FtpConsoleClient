1843896a2011f0a2fcfbddb86d8bfe90fee595fa
Денис Кобзарь <LAYCODE00700@gmail.com>
2019-10-06 23:11:01 +0300

    Создание основных классов
    На данном этапе проекта было создано 3 класса:
    Program (основной клас програмы)
    FtpClient (класс для создания ftp запросов)
    RequestInfo (класс который хранит информацию ответа сервера (код ошибки, сообщение, содержание запрошенного файла))

---
+++ FtpConsoleClient/FtpClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FtpConsoleClient
+{
+    class FtpClient     //  класс для создания ftp запросов
+    {
+    }
+}
---
+++ FtpConsoleClient/RequestInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FtpConsoleClient
+{
+    class RequestInfo   //  класс который хранит информацию ответа сервера (код ошибки, сообщение, содержание запрошенного файла)
+    {
+    }
+}
